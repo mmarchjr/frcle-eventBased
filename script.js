42053c960f28e2acc5728e8835a072d634e48c2c
Colten Kiehne <cokiehne@gmail.com>
2024-11-24 23:00:23 -0600

    Fixed fetch bug

--- script.js
+++ script.js
@@ -9,15 +9,17 @@ const TBA_BASE_URL = "https://www.thebluealliance.com/api/v3";
 const SB_BASE_URL = "https://api.statbotics.io/v3";
 
 var tba;
-
-const table = document.getElementById("table-body");
-
-var preloaded = [];
-var guessed = [];
-
 SBData = {};
 
-window.onload = function () {
+const table = document.getElementById("table-body");
+const input = document.getElementById("guesser");
+
+var guessed = [];
+var guesses = 0;
+var hiddenTeam;
+var banners = 0;
+
+window.onload = async function () {
   fetch(`${TBA_BASE_URL}/status`, TBA_OPTS)
     .then((rsp) => rsp.json())
     .then((json) => tbaStatus(json));
@@ -25,36 +27,106 @@ window.onload = function () {
   // Atomic data, it is more than a day old, we request for new data
   if (localStorage.getItem("sb_timestamp") != null) {
     if (Date.now() - localStorage.getItem("sb_timestamp") > 8.64e7) {
-        console.log("Teams oudated, fetching");
+      console.log("Teams oudated, fetching");
       localStorage.setItem("sb_timestamp", Date.now());
       fetchSBData().then((map) => {
         SBData = map;
+        var teams = map.keys().toArray();
+        hiddenTeam = pickTeam(teams);
+        teams.forEach((entry) => SBData.set(entry[0], entry[1]));
+        populateTeams(SBData);
+        fetchBanners(hiddenTeam).then((b) => banners = b);
       });
     } else {
-        console.log("Teams in localStorage, reading...")
+      console.log("Teams in localStorage, reading...");
       SBData = new Map();
       teams = JSON.parse(localStorage.getItem("sb_teams"));
-
       teams.forEach((entry) => SBData.set(entry[0], entry[1]));
-      console.log("Teams have been read!")
+      teams = SBData.keys().toArray();
+      hiddenTeam = pickTeam(teams);
+      populateTeams(SBData);
+      fetchBanners(hiddenTeam).then((b) => banners = b);
+      console.log("Teams have been read!");
     }
   } else {
     console.log("Fetching teams...");
     localStorage.setItem("sb_timestamp", Date.now());
     fetchSBData().then((map) => {
       SBData = map;
+      var teams = map.keys().toArray();
+      hiddenTeam = pickTeam(teams);
+      fetchBanners(hiddenTeam).then((b) => banners = b);
+      populateTeams(SBData);
     });
   }
 };
 
 window.addEventListener("beforeunload", (event) => {
-    event.preventDefault();
+  //   event.preventDefault();
   if (SBData) {
     localStorage.setItem("sb_teams", JSON.stringify([...SBData]));
   } else {
     cleanCache();
   }
 });
+
+function populateTeams(SBData) {
+  var teams = Array.from(SBData.keys()).sort();
+  hiddenTeam = pickTeam(teams);
+  teams.forEach((team) => {
+    let element = document.createElement("option");
+    element.value = team;
+    document.getElementById("teams").appendChild(element);
+  });
+  console.log("Teams have been populated!");
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && guesser.value != "") {
+    guess(guesser.value);
+    guesser.value = "";
+  }
+});
+
+function pickTeam(teams) {
+  idx = Math.random() * teams.length;
+  return teams.at(idx);
+}
+
+function guess(number) {
+  if (SBData) {
+    if (SBData.get(number) && guessed.indexOf(number) == -1) {
+      guesses += 1;
+      guessed.push(number);
+      teamData = SBData.get(number);
+      fetchBanners(number).then((banners) =>
+        table.insertBefore(
+          buildTableRow([
+            teamData.team,
+            teamData.name,
+            teamData.country,
+            banners,
+            teamData.norm_epa.current,
+            teamData.rookie_year,
+          ]),
+          table.firstChild
+        )
+      );
+
+      guesser.value = "";
+    } else {
+      if (guessed.indexOf(number) != -1) {
+        alert(`Team ${number} has already been guessed!`);
+      } else {
+        alert(
+          `Team ${number} does not exist, or did not compete in the ${tba.current_season} season`
+        );
+      }
+    }
+  } else {
+    alert("Currently fetching data, try again momentarily!");
+  }
+}
 
 function tbaStatus(json) {
   tba = json;
@@ -64,33 +136,8 @@ function tbaStatus(json) {
   }
 }
 
-function readSBData(preload, number) {
-  if (guessed.indexOf(number) != -1) {
-    // has been guessed
-  } else {
-    if (preload) {
-      fetchTeam(number).then((data) => preloaded.push({ data }));
-    } else {
-      fetchTeam(number).then((data) => table.appendChild(buildTableRow(data)));
-    }
-  }
-}
-
-async function fetchTeamData(number) {
-  let simple = null;
+async function fetchBanners(number) {
   let awards = null;
-  let rank = null;
-
-  if (parseInt(number) <= 0 || parseInt(number) > 15000) {
-    // Handle invalid team number here, if needed
-    alert(`Team ${number} does not exist!`);
-    return null;
-  }
-
-  // Fetch team info
-  simple = await fetch(`${TBA_BASE_URL}/team/frc${number}`, TBA_OPTS).then(
-    (rsp) => (rsp.ok ? rsp.json() : null)
-  );
 
   // Fetch awards info
   awards = await fetch(
@@ -98,41 +145,52 @@ async function fetchTeamData(number) {
     TBA_OPTS
   ).then((rsp) => (rsp.ok ? rsp.json() : null));
 
-  rank = await fetch(
-    `${SB_BASE_URL}/team_year/${number}/${tba.current_season}`
-  ).then((rsp) => (rsp.ok ? rsp.json() : null));
-
-  if (simple && awards && rank) {
-    console.log(awards.filter((award) => award.award_number == 1).length);
-    return [
-      simple.team_number,
-      simple.country,
-      awards.filter((award) => award.award_type == 1).length, // Banners
-      rank.epa.ranks.total.rank,
-      simple.rookie_year,
-    ];
-  } else {
-    alert(`Team ${number} does not exist!`);
-    return null;
-  }
-}
-
-function guess(number) {
-  if (SBData) {
-    teamData = SBData.get(number);
-    table.appendChild(buildTableRow([teamData.team]));
-  } else {
-    alert("Currently fetching data, try again momentarily!");
-  }
+  console.log(awards.filter((award) => award.award_number == 1).length);
+  return awards.filter((award) => award.award_type == 1).length;
 }
 
 function buildTableRow(data) {
   var tr = document.createElement("tr");
-  console.log(data);
+
+  sb_array = SBData.get(hiddenTeam);
+  sb_data = [
+    sb_array.team,
+    null,
+    sb_array.country,
+    banners,
+    sb_array.norm_epa.current,
+    sb_array.rookie_year,
+  ];
 
   for (i = 0; i < data.length; ++i) {
     td = document.createElement("td");
+    icon = document.createElement("i");
     td.innerHTML = data[i];
+
+    if ([0, 2, 3, 4, 5].indexOf(i) != -1) {
+      if (i == 2) {
+        if (data[i] != sb_data[i]) {
+          td.classList.add("lower");
+          icon.classList = "fa-solid fa-x";
+        } else {
+          td.classList.add("correct");
+          icon.classList = "fa-solid fa-check";
+        }
+      } else {
+        if (parseInt(data[i]) > parseInt(sb_data[i])) {
+          td.classList.add("lower");
+          icon.classList = "far fa-arrow-alt-circle-down";
+        } else if (parseInt(data[i]) < parseInt(sb_data[i])) {
+          td.classList.add("higher");
+          icon.classList = "far fa-arrow-alt-circle-up";
+        } else {
+          td.classList.add("correct");
+          icon.classList = "fa-solid fa-check";
+        }
+      }
+    }
+
+    td.appendChild(icon);
     tr.appendChild(td);
   }
 
@@ -151,15 +209,18 @@ async function fetchSBData() {
 
   teamMap = new Map();
 
-  teams.forEach(
-    (rsp) =>
-      
-        rsp
-          .json()
-          .then((json) =>
-            json.values().forEach((team) => teamMap.set(team.team, team))
-          )
-      );
+  for (const rsp of teams) {
+    if (rsp.ok) {
+      const json = await rsp.json();
+      json.forEach((team) => {
+        teamMap.set(team.team, team);
+      });
+    } else {
+      console.error(`Error fetching teams: ${rsp.statusText}`);
+    }
+  }
+
+  console.log(teamMap);
 
   return teamMap;
 }
